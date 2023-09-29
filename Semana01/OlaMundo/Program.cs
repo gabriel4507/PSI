e0814dfe24b7447c620efbe4fbaa4a60a62281ed
Gabriel <gabriel45silva07@gmail.com>
2023-09-29 10:18:48 +0100

    adicionar comentarios a ola mundo

--- Semana01/OlaMundo/Program.cs
+++ Semana01/OlaMundo/Program.cs
@@ -2,7 +2,13 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// este programa serve para dizer ola mundo ao utulizador
+    /// </summary>
     class Program
+    /// <summary>
+    /// este método é responsavel por imprimir a mensagem
+    /// </summary>
     {
         static void Main(string[] args)
         {
