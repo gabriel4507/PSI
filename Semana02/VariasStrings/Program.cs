5a507031181206aa346809b99a03ed6032ceb3a9
Gabriel <gabriel45silva07@gmail.com>
2023-10-02 09:27:32 +0100

    Strings

---
+++ Semana02/VariasStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string psi = "psiii";
+            string aula = @"aulass";
+            string b = @"""Uma string entre aspas""";
+            string e = @"\Uma string sem aspas";
+            string t = "\u00A9 e \u2665";
+            string i = @"\u00A9 e \u2665 sem UTF";
+
+            Console.WriteLine(psi);
+            Console.WriteLine(aula);
+            Console.WriteLine(b);
+            Console.WriteLine(e);
+            Console.WriteLine(t);
+            Console.WriteLine(i);
+        }
+    }
+}
