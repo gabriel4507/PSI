2520869b4329bfa7b2860295b274ca0cfefc46be
Replit user <>
2024-01-08 08:54:30 +0000

    Testar Erro

---
+++ Semana09/TesteErro/TesteErro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TesteErro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          Console.Write("Insere um número inteiro: ")
+          int i = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine($"Número inserido: {i}");
+
+             Console.WriteLine("Hello, World!");
+          
+        }
+    }
+}
