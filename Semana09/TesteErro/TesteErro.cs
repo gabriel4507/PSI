c129a2087346b3f260babb71d3436ef0c33e8ad3
Replit user <>
2024-01-10 08:52:51 +0000

    Testar Erros

--- Semana09/TesteErro/TesteErro.cs
+++ Semana09/TesteErro/TesteErro.cs
@@ -12,15 +12,28 @@ namespace TesteErro
             int i = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Número inserido: {i}");
           }
+
+          catch(FormatException)
+          {    
+            Console.WriteLine("Não é um número intereiro!");
+          }
+
+          catch(OverflowException)
+          {
+            Console.WriteLine("Número muito grande ou pequeno!");
+          }
           
           catch (Exception e)
           {
             Console.WriteLine("Ocorreu o seguinte problema:" + e.Message);
           }
 
+          finally
+          {
+            Console.WriteLine("Programa finalizado!");
+          }
 
-            Console.WriteLine("Hello, World!");
-          
+        
         }
     }
 }
